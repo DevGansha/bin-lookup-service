36e60c7e1f64da839ba6a9fcfcc7027692eb3348
DevGansha <ganshakumari97@gmail.com>
2025-07-02 11:44:36 +1000

    Initial commit

---
+++ src/main/java/org/example/binlookupservice/BinLookupServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.binlookupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinLookupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinLookupServiceApplication.class, args);
+    }
+
+}
